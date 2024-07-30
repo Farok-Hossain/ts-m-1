aa3bc15c3be2bbcf781f846fda080db7d43434e4
Farok Hossain <farokafs7@gmail.com>
2024-07-30 23:20:51 +0600

    aliasfile updated

--- src/alias.ts
+++ src/alias.ts
@@ -17,3 +17,25 @@ const crush2: CrushType = {
   profession: "next.js developer",
   address: "australia",
 };
+
+type CourseType = string;
+
+const courseNames: CourseType = "Next Level Web Development";
+
+const adds = (n1: number, n2: number): number => {
+  return n1 + n2;
+};
+
+const sum = adds(12, 23);
+console.log(sum);
+
+type OperationType = (x: number, y: number) => number;
+
+const calculate = (nm1: number, nm2: number, operation: OperationType) => {
+  return operation(nm1, nm2);
+};
+
+const result1 = calculate(11, 13, (x, y) => x + y);
+const result2 = calculate(11, 13, (x, y) => x - y);
+const result3 = calculate(11, 13, (x, y) => x * y);
+console.log(`result1: ${result1}\nresult2: ${result2}\nresult3: ${result3}`);
